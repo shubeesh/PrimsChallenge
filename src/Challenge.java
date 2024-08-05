b61e5d7c510449db20cd5e710bbee959fb0fb421
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-08-05 13:46:20 -0400

    challenges3

--- src/Challenge.java
+++ src/Challenge.java
@@ -2,11 +2,8 @@ public class Challenge {
 
     public static void main(String[] args) {
         Byte myByte = 8;
-        System.out.println(myByte);
         Short myShort = 32;
-        System.out.println(myShort);
         Integer myInt = 64;
-        System.out.println(myInt);
         int Long =(myByte * myShort * myInt) * 5;
 
     }
